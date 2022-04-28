3d29b0d4cb8f93ed99e13989faef9f03840bc8ff
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-04-28 20:58:47 -0300

    2.6: The Phonebook Step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ id: 1, name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    
-  }
+    const newObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    //console.log(persons.length + 1);
+    setPersons(persons.concat(newObject));
+    setNewName('');
+  };
 
   const handleInputChange = (e) => {
-    console.log(e.target.value);
-    setNewName(e.target.value);
+    return setNewName(e.target.value);
   };
 
   return (
@@ -20,18 +24,19 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: 
-          <input 
-          value={newName} 
-          onChange={handleInputChange} />
+          name:
+          <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {/*display array persons*/}
-      <div>...</div>
+      <div>
+        {persons.map((item) => (
+          <p key={item.id}>{item.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
